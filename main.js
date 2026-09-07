2f8ec4cf0a7174bb08997d9126fe66f748b96c4e
Teemu Hakoniemi <hakoniemi.teemu@gmail.com>
2026-09-07 12:54:23 +0300

    task 2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const populationURL = "https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/vaerak/11ra.px";
+const populationURL =
+    "https://pxdata.stat.fi/PxWeb/api/v1/fi/StatFin/vaerak/11ra.px";
 
 const fetchStatFinData = async (URL, body) => {
     const response = await fetch(URL, {
@@ -8,15 +9,44 @@ const fetchStatFinData = async (URL, body) => {
         },
         body: JSON.stringify(body)
     });
+
     return await response.json();
 };
 
 const initializeCode = async () => {
-    const populationBody = await (await fetch("/population_query.json")).json();
+    const populationBody =
+        await (await fetch("/population_query.json")).json();
 
     const [populationData] = await Promise.all([
-        fetchStatFinData(populationUrl, populationBody)
+        fetchStatFinData(populationURL, populationBody)
     ]);
 
-    setupTable(populationData, populationBody);
-}
+    setupTable(populationData);
+};
+
+const setupTable = (populationData) => {
+    const municipalities =
+        populationData.dimension.alue_23_20260101.category.label;
+
+    const populations =
+        populationData.value;
+
+    const tableBody = document.querySelector("tbody");
+
+    Object.values(municipalities).forEach((municipality, index) => {
+        const row = document.createElement("tr");
+
+        const municipalityCell = document.createElement("td");
+        municipalityCell.textContent = municipality;
+
+        const populationCell = document.createElement("td");
+        populationCell.textContent = populations[index];
+
+        row.appendChild(municipalityCell);
+        row.appendChild(populationCell);
+
+        tableBody.appendChild(row);
+    });
+};
+
+initializeCode();
